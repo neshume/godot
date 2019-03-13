d8cebd4dff050f44996b117bae9d98ae2def9a45
alex.andel <neshume@gmail.com>
2019-03-13 21:46:14 +0200

    Mono support
    Mono support

--- create-app.sh
+++ create-app.sh
@@ -1,4 +1,4 @@
 cp -r misc/dist/osx_tools.app ./Godot.app
 mkdir -p Godot.app/Contents/MacOS
-cp bin/godot.osx.tools.64 Godot.app/Contents/MacOS/Godot
+cp bin/godot.osx.tools.64.mono Godot.app/Contents/MacOS/Godot
 chmod +x Godot.app/Contents/MacOS/Godot
